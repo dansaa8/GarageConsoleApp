6c820bf6693a293eb167de913ab82d9d0faaec3d
dan saa <dansaa8@protonmail.com>
2024-10-01 14:46:37 +0200

    Add a Garage test, to make sure we get a false back from Add method, when the garage is full.

---
+++ GarageConsoleApp.Tests/Entities/Garage/GarageTest.cs
@@ -0,0 +1,55 @@
+using GarageConsoleApp.Entities;
+using GarageConsoleApp.Entities.Garage;
+
+namespace GarageConsoleApp.Tests.Entities.Garage;
+
+public class GarageTest
+{
+    [Fact]
+    public void GarageConstructorTest()
+    {
+        // Arrange
+        int garageSize = 8;
+
+        // Act
+        Garage<Vehicle> garage = new Garage<Vehicle>(garageSize);
+
+        // Assert
+        Assert.NotNull(garage);
+    }
+
+    [Fact]
+    public void AddVehicle_Return_True()
+    {
+        // Arrange
+        int garageSize = 8;
+        Car car = new Car("abc123", 4, VehicleColor.Black, true);
+
+        // Act
+        Garage<Vehicle> garage = new Garage<Vehicle>(garageSize);
+        bool vehicleIsAdded = garage.Add(car);
+
+        // Assert
+        Assert.True(vehicleIsAdded);
+    }
+
+    [Fact]
+    public void AddVehicleToFullGarage_Return_False()
+    {
+        // Arrange
+        int garageSize = 1;
+        Car car1 = new Car("def456", 4, VehicleColor.Black, true);
+        Car car2 = new Car("abc123", 4, VehicleColor.Black, true);
+
+
+        // Act
+        Garage<Vehicle> garage = new Garage<Vehicle>(garageSize);
+        bool car1IsAdded = garage.Add(car1);
+        bool car2IsAdded = garage.Add(car2);
+
+
+        // Assert
+        Assert.True(car1IsAdded);
+        Assert.False(car2IsAdded);
+    }
+}
